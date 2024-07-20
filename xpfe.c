df89deaeae3028c4cc03da2d1059d7a13c80c244
Kenji Aoyama <kenji.aoyama@gmail.com>
2024-07-20 10:45:33 +0900

    Delete unused includes.

--- xpfe.c
+++ xpfe.c
@@ -19,15 +19,10 @@
  */
 
 #include <err.h>
-#include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <termios.h>
-
-#include <sys/stat.h>
 
 #include "config.h"
 
